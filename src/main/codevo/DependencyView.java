77cd2766e26962e308f3e2bda9e0c0101893a030
Zhongpeng Lin <lin.zhp@gmail.com>
2014-06-08 22:40:21 -0700

    Created an empty view

---
+++ src/main/codevo/DependencyView.java
@@ -0,0 +1,24 @@
+package codevo;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class DependencyView extends ViewPart {
+
+	public DependencyView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
